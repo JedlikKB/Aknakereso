a4f9ce3174629850e582712e4613e69ac32baa24
Kozma.Barnabás <Kozma.Barnabs@C102-09>
2020-02-04 14:06:41 +0100

    Feltöltés

--- Aknakereso/Aknakereso/Program.cs
+++ Aknakereso/Aknakereso/Program.cs
@@ -8,18 +8,44 @@ namespace Aknakereso
 {
     class Program
     {
-        char[,] pálya = new char[10, 10];
 
         static void Main(string[] args)
         {
-            Feltöltés();
-
-            Console.ReadKey() ;
+            char[,] pálya = new char[10, 10];
+            Feltöltés(pálya);
+            Kirajzoló(pálya);
+            Console.ReadKey();
         }
 
-         static void Feltöltés()
+        private static void Kirajzoló(char[,] pálya)
         {
-            throw new NotImplementedException();
+
         }
+
+        private static void Feltöltés(char[,] pálya)
+        {
+            Random gép = new Random();
+            for (int i = 0; i < pálya.GetLength(0); i++)
+            {
+                for (int j = 0; j < pálya.GetLength(1); j++)
+                {
+                    pálya[i, j] = '_';
+                }
+            }
+            int sor, oszlop;
+            for (int i = 0; i < 10; i++)
+            {
+                do
+                {
+                    sor = gép.Next(10);
+                    oszlop = gép.Next(10);
+                } while (pálya[sor,oszlop]=='B');
+                pálya[sor, oszlop] = 'B';
+            }
+        }
+
+
+
+
     }
 }
